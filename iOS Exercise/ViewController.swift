6052d77a9858e7c6314f34112ebaae7fdead6fee
allenlin0105 <b08902053@csie.ntu.edu.tw>
2021-12-30 09:22:54 +0800

    Initial Commit

---
+++ iOS Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Exercise
+//
+//  Created by 林承濬 on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
