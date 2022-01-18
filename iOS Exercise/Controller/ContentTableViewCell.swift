51316a4a4f41c953b86569302907b750e3dd40b7
承濬 <allenlin@cathayholdings.com.tw>
2022-01-18 16:29:40 +0800

    Let the feature's top align to the bottom of the image or the location

--- iOS Exercise/Controller/ContentTableViewCell.swift
+++ iOS Exercise/Controller/ContentTableViewCell.swift
@@ -13,12 +13,15 @@ class ContentTableViewCell: UITableViewCell {
     @IBOutlet weak var plantName: UILabel!
     @IBOutlet weak var plantLocation: UILabel!
     @IBOutlet weak var plantFeature: UILabel!
+    @IBOutlet weak var innerStackView: UIStackView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.plantImage.contentMode = .top
         self.plantImage.clipsToBounds = true
+        
+        innerStackView.setCustomSpacing(0, after: plantLocation)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
