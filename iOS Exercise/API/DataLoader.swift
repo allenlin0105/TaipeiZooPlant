3aafe873c0fabf6473ac0ecb5a3f78e6821524be
承濬 <allenlin@cathayholdings.com.tw>
2022-01-21 10:56:07 +0800

    Add DataLoader

---
+++ iOS Exercise/API/DataLoader.swift
@@ -0,0 +1,31 @@
+//
+//  DataLoader.swift
+//  iOS Exercise
+//
+//  Created by allen on 2022/1/20.
+//
+
+import UIKit
+import Alamofire
+
+class DataLoader {
+    func urlConstructor(scheme: String, host: String, path: String, parameters: [String: String]) -> URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = scheme
+        urlComponents.host = host
+        urlComponents.path = path
+        urlComponents.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        return urlComponents.url
+    }
+    
+    func loadData(requestUrl: URL, completionHandler: @escaping ((Data?) -> Void)) {
+        AF.request(requestUrl).responseData { response in
+            switch response.result {
+            case .success:
+                completionHandler(response.data)
+            case .failure:
+                completionHandler(nil)
+            }
+        }
+    }
+}
