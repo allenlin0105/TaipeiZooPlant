b7b78b921980ce14b2ad08c5fdf7379907c0e453
承濬 <allenlin@cathayholdings.com.tw>
2022-01-27 11:38:25 +0800

    [Refactor] Add private extension for ContentViewModel

--- iOS ExerciseTests/Content/TestContentViewModel.swift
+++ iOS ExerciseTests/Content/TestContentViewModel.swift
@@ -19,56 +19,51 @@ class TestContentViewModel: XCTestCase {
     
     func test_setupUrl_withOneRequest_receiveUrlWithOffsetEqualsZero() {
         let (sut, _) = makeSUT(with: [.successWithJSON])
-        sut.requestPlantData(at: 0)
+        sut.requestProcedure()
         
         XCTAssertEqual(sut.apiString, "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f18de02f-b6c9-47c0-8cda-50efad621c14&limit=20&offset=0")
     }
         
     func test_requestDataTwice_withTwoSameRequestsAndNoFail_onlyReceiveDataOnce() {
         let (sut, stub) = makeSUT(with: [.successWithJSON, .successWithJSON], totalStub: 1)
-        sut.requestPlantData(at: 0)
-        sut.requestPlantData(at: 0)
-
-        XCTAssertEqual(sut.plantDataModel.plantDataList, stub)
+        sut.requestProcedure(for: [0, 0])
+        
+        XCTAssertEqual(sut.dataList(), stub)
         XCTAssertEqual(sut.alreadyRequestOffset, 0)
     }
     
     func test_requestDataTwice_withTwoDifferentRequestsAndNoFail_receiveCorrectData() {
         let (sut, stub) = makeSUT(with: [.successWithJSON, .successWithJSON], totalStub: 2)
-        sut.requestPlantData(at: 0)
-        sut.requestPlantData(at: 20)
+        sut.requestProcedure(for: [0, 20])
         
-        XCTAssertEqual(sut.plantDataModel.plantDataList, stub)
+        XCTAssertEqual(sut.dataList(), stub)
         XCTAssertEqual(sut.alreadyRequestOffset, 20)
     }
     
     func test_requestDataTwice_withTwoDifferentRequestsButOneNetworkFail_onlyReceiveDataOnce() {
         let (firstSUT, stub) = makeSUT(with: [.successWithJSON, .networkFailure], totalStub: 1)
-        firstSUT.requestPlantData(at: 0)
-        firstSUT.requestPlantData(at: 20)
+        firstSUT.requestProcedure(for: [0, 20])
         
         let (secondSUT, _) = makeSUT(with: [.networkFailure, .successWithJSON], totalStub: 1)
-        secondSUT.requestPlantData(at: 0)
-        secondSUT.requestPlantData(at: 0)
+        secondSUT.requestProcedure(for: [0, 0])
         
-        XCTAssertEqual(firstSUT.plantDataModel.plantDataList, stub)
-        XCTAssertEqual(secondSUT.plantDataModel.plantDataList, stub)
+        XCTAssertEqual(firstSUT.dataList(), stub)
+        XCTAssertEqual(secondSUT.dataList(), stub)
         XCTAssertEqual(firstSUT.alreadyRequestOffset, 0)
         XCTAssertEqual(secondSUT.alreadyRequestOffset, 0)
     }
     
     func test_requestDataTwice_withTwoDifferentRequestsButBothFail_receiveNetworkError() {
         let (sut, _) = makeSUT(with: [.networkFailure, .networkFailure])
-        sut.requestPlantData(at: 0)
-        sut.requestPlantData(at: 0)
+        sut.requestProcedure(for: [0, 0])
         
-        XCTAssertEqual(sut.plantDataModel.plantDataList, [])
+        XCTAssertEqual(sut.dataList(), [])
         XCTAssertEqual(sut.alreadyRequestOffset, -20)
     }
     
     func test_lastRequest_withAllDataReceived_setFinishAllAccessToTrue() {
         let (sut, _) = makeSUT(with: [.decodeFailure])
-        sut.requestPlantData(at: 1000)
+        sut.requestProcedure(for: [1000])
         
         XCTAssertTrue(sut.finishAllAccess)
     }
@@ -78,26 +73,26 @@ class TestContentViewModel: XCTestCase {
         sut.requestPlantData(at: 0)
         sut.requestImage(at: 0)
         
-        XCTAssertNil(sut.plantDataModel.plantDataList.first?.imageURL)
-        XCTAssertNil(sut.plantDataModel.plantDataList.first?.image)
+        XCTAssertNil(sut.dataList().first?.imageURL)
+        XCTAssertNil(sut.dataList().first?.image)
     }
     
     func test_requestImage_withImageUrl_receiveCorrectImage() {
         let (sut, stub) = makeSUT(with: [.successWithJSON, .successWithImage], totalStub: 1, stubWithImage: true)
-        sut.requestPlantData(at: 0)
+        sut.requestProcedure()
         sut.requestImage(at: 0)
         
-        XCTAssertNotNil(sut.plantDataModel.plantDataList.first?.image)
-        XCTAssertEqual(sut.plantDataModel.plantDataList.first?.image?.pngData(), stub.first?.image?.pngData())
+        XCTAssertNotNil(sut.dataList().first?.image)
+        XCTAssertEqual(sut.dataList().first?.image?.pngData(), stub.first?.image?.pngData())
     }
     
     func test_requestImage_withImageUrlButNetworkFail_imageStillNil() {
         let (sut, _) = makeSUT(with: [.successWithJSON, .networkFailure])
-        sut.requestPlantData(at: 0)
+        sut.requestProcedure()
         sut.requestImage(at: 0)
         
-        XCTAssertNotNil(sut.plantDataModel.plantDataList.first?.imageURL)
-        XCTAssertNil(sut.plantDataModel.plantDataList.first?.image)
+        XCTAssertNotNil(sut.dataList().first?.imageURL)
+        XCTAssertNil(sut.dataList().first?.image)
     }
     
     // MARK: - Helper
@@ -187,6 +182,21 @@ class TestContentViewModel: XCTestCase {
             """
             let data: Data? = jsonString.data(using: .utf8)
             return data!
+        }
+    }
+}
+
+// MARK: - ContentViewModel Private Extension
+
+private extension ContentViewModel {
+
+    func dataList() -> [PlantData] {
+        return self.plantDataModel.plantDataList
+    }
+    
+    func requestProcedure(for offset: [Int] = [0]) {
+        for i in offset {
+            self.requestPlantData(at: i)
         }
     }
 }
