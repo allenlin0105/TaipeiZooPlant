9fef14fc53e836f36b0308c048f5fd1b0f026b36
承濬 <allenlin@cathayholdings.com.tw>
2022-01-28 19:01:44 +0800

    [Task] Update SceneDelegate to show main storyboard

--- iOS Exercise/SceneDelegate.swift
+++ iOS Exercise/SceneDelegate.swift
@@ -16,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
 
@@ -26,13 +25,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window?.makeKeyAndVisible()
     }
     
-    func makeContentViewController() -> ContentViewController {
+    private func makeContentViewController() -> ContentViewController {
         let mainStoryboard = UIStoryboard.init(name: GlobalStrings.mainStoryboardIdentifier, bundle: nil)
-        let viewController =  mainStoryboard.instantiateViewController(withIdentifier: GlobalStrings.mainStoryboardIdentifier) as! ContentViewController
-//        let dataLoader = DataLoader()
-//        viewController.viewModel = ContentViewModel(apiString: GlobalStrings.baseAPIString)
-//        viewController.viewModel = ContentViewModel(dataLoader: dataLoader)
-        
+        let viewController = mainStoryboard.instantiateViewController(withIdentifier: GlobalStrings.mainStoryboardIdentifier) as! ContentViewController
+        viewController.viewModel = ContentViewModel(dataLoader: DataLoader())
+
         return viewController
     }
 
