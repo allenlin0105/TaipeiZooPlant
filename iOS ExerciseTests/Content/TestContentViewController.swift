77d6b631f2e148d365c7b449b07d09756b5cbc9e
承濬 <allenlin@cathayholdings.com.tw>
2022-01-28 13:42:40 +0800

    [Refactor] create makeSUT function

--- iOS ExerciseTests/Content/TestContentViewController.swift
+++ iOS ExerciseTests/Content/TestContentViewController.swift
@@ -11,27 +11,43 @@ import XCTest
 class TestContentViewController: XCTestCase {
     
     func test_viewDidLoad_setupViewModelDelegate() {
-        let mock = DataLoaderMock(apiCondition: [.successWithJSON], image: nil)
-        let spy = ContentViewModel(dataLoader: mock)
-        let sut = ContentViewController(viewModel: spy)
-        
+        let (sut, spy, _) = makeSUT()
         _ = sut.view
         
         XCTAssertTrue(sut.isEqualTo(spy.delegate!))
     }
     
     func test_viewDidLoad_withNoNetworkFail_receiveCorrectData() {
-        let mock = DataLoaderMock(apiCondition: [.successWithJSON], image: nil)
-        let spy = ContentViewModel(dataLoader: mock)
-        let sut = ContentViewController(viewModel: spy)
-        
+        let (sut, spy, stub) = makeSUT(totalStub: 1)
         _ = sut.view
         
-        let stub = makeStub(totalStub: 1, imageUrl: mock.imageUrl, image: mock.image)
         XCTAssertEqual(spy.plantDataModel.plantDataList, stub)
     }
     
     // MARK: - Helper
+    
+    func makeSUT(apiCondition: [APICondition] = [.successWithJSON], totalStub: Int = 0,  withImageUrl: Bool = true, stubWithImage: Bool = false) -> (ContentViewController, ContentViewModel, [PlantData]) {
+        var mock = DataLoaderMock()
+        if withImageUrl && stubWithImage {
+            mock = DataLoaderMock(apiCondition: apiCondition)
+        } else if !withImageUrl {
+            mock = DataLoaderMock(apiCondition: apiCondition, imageUrl: "")
+        } else if !stubWithImage {
+            mock = DataLoaderMock(apiCondition: apiCondition, image: nil)
+        } else {
+            mock = DataLoaderMock(apiCondition: apiCondition, imageUrl: "", image: nil)
+        }
+        
+        let spy = ContentViewModel(dataLoader: mock)
+        
+        let mainStoryboard = UIStoryboard.init(name: GlobalStrings.mainStoryboardIdentifier, bundle: nil)
+        let sut = mainStoryboard.instantiateViewController(withIdentifier: GlobalStrings.mainStoryboardIdentifier) as! ContentViewController
+        sut.viewModel = spy
+        
+        let stub = makeStub(totalStub: totalStub, imageUrl: mock.imageUrl, image: mock.image)
+        
+        return (sut, spy, stub)
+    }
 }
 
 // MARK: - Private extension for ContentProtocol
