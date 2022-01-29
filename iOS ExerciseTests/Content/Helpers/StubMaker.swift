bf1a075339dabfb3b9d9951975c43863cec12366
allenlin0105 <b08902053@csie.ntu.edu.tw>
2022-01-29 14:06:21 +0800

    [Refactor] Make the initialization of stub clear

--- iOS ExerciseTests/Content/Helpers/StubMaker.swift
+++ iOS ExerciseTests/Content/Helpers/StubMaker.swift
@@ -11,7 +11,12 @@ import UIKit
 public func makeStub(totalStub: Int, imageURL: String, image: UIImage?) -> [PlantData] {
     var stub: [PlantData] = []
     for i in 0..<totalStub {
-        stub += [PlantData].init(repeating: PlantData(name: "name\(i)", location: "location\(i)", feature: "feature\(i)", imageURL: URL(string: imageURL), image: image), count: 20)
+        stub += [PlantData].init(repeating: PlantData(name: "name\(i)",
+                                                      location: "location\(i)",
+                                                      feature: "feature\(i)",
+                                                      imageURL: URL(string: imageURL),
+                                                      image: image),
+                                 count: 20)
     }
     
     return stub
