74965eb12bdb24af78e5b362feb091b8fc384d70
承濬 <allenlin@cathayholdings.com.tw>
2022-02-16 16:21:37 +0800

    [Refactor] Remove DecodedPlantData and move decoding into PlantData

--- iOS Exercise/Content/API/DataMapper.swift
+++ iOS Exercise/Content/API/DataMapper.swift
@@ -12,16 +12,8 @@ class DataMapper {
     static func mapTextData(data: Data) -> [PlantData]? {
         let decoder = JSONDecoder()
         do {
-            let decodedData = try decoder.decode(DecodedPlantModel.self, from: data)
-            let responseResult = decodedData.result.results
-            let usableResult = responseResult.map {
-                PlantData(name: $0.F_Name_Ch,
-                          location: $0.F_Location,
-                          feature: $0.F_Feature,
-                          imageURL: URL(string: $0.F_Pic01_URL) ?? nil,
-                          image: nil)
-            }
-            return usableResult
+            let decodedData = try decoder.decode(PlantModel.self, from: data)
+            return decodedData.plantDataList
         } catch {
             return nil
         }
