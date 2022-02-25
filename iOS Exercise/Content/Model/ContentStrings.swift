c394f721f5ffff7b5ea6ca9bd4f9f08feb7e72df
承濬 <allenlin@cathayholdings.com.tw>
2022-02-25 11:19:53 +0800

    [Other] Add error image

--- iOS Exercise/Content/Model/ContentStrings.swift
+++ iOS Exercise/Content/Model/ContentStrings.swift
@@ -11,4 +11,5 @@ struct ContentStrings {
     static let contentCellIdentifier = "ContentTableViewCell"
     static let errorCellIdentifier = "ErrorTableViewCell"
     static let baseAPIString = "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f18de02f-b6c9-47c0-8cda-50efad621c14&limit=20"
+    static let errorImageName = "ErrorImage"
 }
