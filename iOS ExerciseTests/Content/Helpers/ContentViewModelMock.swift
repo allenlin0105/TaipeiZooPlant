0ba8b6745fb813481cb7db8ca80ac61ae797ceac
承濬 <allenlin@cathayholdings.com.tw>
2022-03-29 11:40:26 +0800

    [Task] Add content view model mock

---
+++ iOS ExerciseTests/Content/Helpers/ContentViewModelMock.swift
@@ -0,0 +1,31 @@
+//
+//  ContentViewModelMock.swift
+//  iOS ExerciseTests
+//
+//  Created by allen on 2022/3/1.
+//
+
+import Foundation
+@testable import iOS_Exercise
+
+class ContentViewModelMock: ContentViewModelProtocol {
+
+    var plantDataModel: PlantModel = PlantModel(plantDataList: [])
+    var requestPlantDataStatus: APIStatus = .success
+    var dataCount: Int = 0
+    var delegate: ContentViewProtocol?
+    
+    required init(dataLoader: DataLoaderProtocol, delegate: ContentViewProtocol) {
+        self.delegate = delegate
+    }
+    
+    func makeAPIString(offset: Int) -> String {
+        return ""
+    }
+    
+    func requestPlantData(at offset: Int) {
+    }
+    
+    func requestImage(at index: Int) {
+    }
+}
